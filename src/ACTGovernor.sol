47713ab8538955d878ce39a9ecd0265f8ab6141f
illia <illia@acx.net>
2025-10-13 13:46:40 +0700

    feat: Basic Governor contract for DAO

---
+++ src/ACTGovernor.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Governor} from "@openzeppelin/contracts/governance/Governor.sol";
+import {GovernorSettings} from "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import {GovernorCountingSimple} from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import {GovernorVotes} from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import {GovernorVotesQuorumFraction} from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+
+/**
+ * @title ACTGovernor
+ * @dev Basic OpenZeppelin Governor implementation for ACT token
+ */
+contract ACTGovernor is
+    Governor,
+    GovernorSettings,
+    GovernorCountingSimple,
+    GovernorVotes,
+    GovernorVotesQuorumFraction
+{
+    /**
+     * @dev Constructor for ACT Governor
+     * @param _token The ACT token address (must implement IVotes/ERC20Votes)
+     * @param _votingDelay Delay (in blocks) between proposal creation and voting start
+     * @param _votingPeriod Duration (in blocks) for voting on proposals
+     * @param _proposalThreshold Minimum number of votes required to create a proposal
+     * @param _quorumPercentage Percentage of total supply required for quorum (e.g., 4 for 4%)
+     */
+    constructor(
+        IVotes _token,
+        uint48 _votingDelay,
+        uint32 _votingPeriod,
+        uint256 _proposalThreshold,
+        uint256 _quorumPercentage
+    )
+        Governor("ACT Governor")
+        GovernorSettings(_votingDelay, _votingPeriod, _proposalThreshold)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(_quorumPercentage)
+    {}
+
+    function votingDelay()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
+
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(Governor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+}
---
+++ test/ACTGovernor.t.sol
@@ -0,0 +1,235 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {ACT} from "../src/ACT.sol";
+import {ACTGovernor} from "../src/ACTGovernor.sol";
+import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
+import {console} from "forge-std/console.sol";
+
+// forge test --match-contract ACTGovernorTest -vv
+contract ACTGovernorTest is Test {
+    ACT public token;
+    ACTGovernor public governor;
+
+    address public deployer;
+    address public voter1;
+    address public voter2;
+    address public voter3;
+
+    uint256 public constant INITIAL_SUPPLY = 10_000_000 * 10**18;
+    uint48 public constant VOTING_DELAY = 1;
+    uint32 public constant VOTING_PERIOD = 50;
+    uint256 public constant PROPOSAL_THRESHOLD = 0;
+    uint256 public constant QUORUM_PERCENTAGE = 4;
+
+    function setUp() public {
+        deployer = address(this);
+        voter1 = makeAddr("voter1");
+        voter2 = makeAddr("voter2");
+        voter3 = makeAddr("voter3");
+
+        token = new ACT("ACT Token", "ACT", INITIAL_SUPPLY);
+
+        governor = new ACTGovernor(
+            token,
+            VOTING_DELAY,
+            VOTING_PERIOD,
+            PROPOSAL_THRESHOLD,
+            QUORUM_PERCENTAGE
+        );
+
+        token.transfer(voter1, 1_000_000 * 10**18);
+        token.transfer(voter2, 500_000 * 10**18);
+        token.transfer(voter3, 100_000 * 10**18);
+
+        vm.prank(voter1);
+        token.delegate(voter1);
+
+        vm.prank(voter2);
+        token.delegate(voter2);
+
+        vm.prank(voter3);
+        token.delegate(voter3);
+
+        vm.roll(block.number + 1);
+    }
+
+    function test_GovernorDeployment() public view {
+        assertEq(governor.name(), "ACT Governor");
+        assertEq(address(governor.token()), address(token));
+        assertEq(governor.votingDelay(), VOTING_DELAY);
+        assertEq(governor.votingPeriod(), VOTING_PERIOD);
+        assertEq(governor.proposalThreshold(), PROPOSAL_THRESHOLD);
+    }
+
+    function test_QuorumCalculation() public view {
+        uint256 expectedQuorum = (INITIAL_SUPPLY * QUORUM_PERCENTAGE) / 100;
+        assertEq(governor.quorum(block.number - 1), expectedQuorum);
+    }
+
+    function test_CreateProposal() public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = "Proposal #1: Test proposal";
+
+        targets[0] = address(token);
+        values[0] = 0;
+        calldatas[0] = "";
+
+        vm.prank(voter1);
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        assertTrue(proposalId != 0);
+        assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Pending));
+    }
+
+    function test_VoteOnProposal() public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = "Proposal #1: Test proposal";
+
+        targets[0] = address(token);
+        values[0] = 0;
+        calldatas[0] = "";
+
+        vm.prank(voter1);
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        vm.roll(block.number + VOTING_DELAY + 1);
+        assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Active));
+
+        vm.prank(voter1);
+        governor.castVote(proposalId, 1);
+
+        (uint256 againstVotes, uint256 forVotes, uint256 abstainVotes) = governor.proposalVotes(proposalId);
+        assertEq(forVotes, 1_000_000 * 10**18);
+        assertEq(againstVotes, 0);
+        assertEq(abstainVotes, 0);
+    }
+
+    function test_ProposalSucceeds() public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = "Proposal #1: Test proposal";
+
+        targets[0] = address(token);
+        values[0] = 0;
+        calldatas[0] = "";
+
+        vm.prank(voter1);
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        vm.prank(voter1);
+        governor.castVote(proposalId, 1);
+
+        vm.prank(voter2);
+        governor.castVote(proposalId, 1);
+
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Succeeded));
+    }
+
+    function test_ProposalDefeated() public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = "Proposal #1: Test proposal";
+
+        targets[0] = address(token);
+        values[0] = 0;
+        calldatas[0] = "";
+
+        vm.prank(voter1);
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        vm.prank(voter1);
+        governor.castVote(proposalId, 0);
+
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Defeated));
+    }
+
+    function test_QuorumNotReached() public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = "Proposal #1: Test proposal";
+
+        targets[0] = address(token);
+        values[0] = 0;
+        calldatas[0] = "";
+
+        vm.prank(voter1);
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        vm.prank(voter3);
+        governor.castVote(proposalId, 1);
+
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Defeated));
+    }
+
+    function test_GetVotes() public view {
+        uint256 voter1Votes = token.getVotes(voter1);
+        uint256 voter2Votes = token.getVotes(voter2);
+        uint256 voter3Votes = token.getVotes(voter3);
+
+        assertEq(voter1Votes, 1_000_000 * 10**18);
+        assertEq(voter2Votes, 500_000 * 10**18);
+        assertEq(voter3Votes, 100_000 * 10**18);
+    }
+
+    function test_CannotVoteTwice() public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = "Proposal #1: Test proposal";
+
+        targets[0] = address(token);
+        values[0] = 0;
+        calldatas[0] = "";
+
+        vm.prank(voter1);
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        vm.prank(voter1);
+        governor.castVote(proposalId, 1);
+
+        vm.prank(voter1);
+        vm.expectRevert();
+        governor.castVote(proposalId, 1);
+    }
+
+    function test_CannotVoteBeforeActive() public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = "Proposal #1: Test proposal";
+
+        targets[0] = address(token);
+        values[0] = 0;
+        calldatas[0] = "";
+
+        vm.prank(voter1);
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        vm.prank(voter1);
+        vm.expectRevert();
+        governor.castVote(proposalId, 1);
+    }
+}
