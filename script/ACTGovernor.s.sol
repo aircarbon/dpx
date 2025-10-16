0344add8d5db478fe6b1ea15a998f7c65ca4a1e0
illia <illia@acx.net>
2025-10-16 17:22:07 +0700

    wip

--- script/ACTGovernor.s.sol
+++ script/ACTGovernor.s.sol
@@ -9,27 +9,35 @@ import {console} from "forge-std/console.sol";
 // forge script script/ACTGovernor.s.sol --rpc-url sepolia --broadcast
 contract ACTGovernorScript is Script {
     function run() public {
-        string memory root = vm.projectRoot();
-        string memory path = string.concat(
-            root,
-            "/broadcast/ACT.s.sol/",
-            vm.toString(block.chainid),
-            "/run-latest.json"
-        );
-        string memory json = vm.readFile(path);
-        address actTokenAddress = abi.decode(
-            vm.parseJson(json, ".transactions[0].contractAddress"),
-            (address)
-        );
+        // string memory root = vm.projectRoot();
+        // string memory path = string.concat(
+        //     root,
+        //     "/broadcast/ACT.s.sol/",
+        //     vm.toString(block.chainid),
+        //     "/run-latest.json"
+        // );
+        // string memory json = vm.readFile(path);
+        // address actTokenAddress = abi.decode(
+        //     vm.parseJson(json, ".transactions[0].contractAddress"),
+        //     (address)
+        // );
 
+        address actTokenAddress = address(vm.envAddress("ACT_TOKEN_ADDRESS"));
         uint48 votingDelay = uint48(vm.envOr("DAO_VOTING_DELAY", uint256(7200)));
         uint32 votingPeriod = uint32(vm.envOr("DAO_VOTING_PERIOD", uint256(50400)));
         uint256 proposalThresholdWhole = vm.envOr("DAO_PROPOSAL_THRESHOLD", uint256(1000));
         uint256 proposalThreshold = proposalThresholdWhole * 10**18;
         uint256 quorumPercentage = vm.envOr("DAO_QUORUM_PERCENTAGE", uint256(4));
 
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+        uint256 deployerPrivateKey;
+
+        try vm.envUint("PRIVATE_KEY") returns (uint256 pk) {
+            deployerPrivateKey = pk;
+            vm.startBroadcast(deployerPrivateKey);
+        } catch {
+            // If no private key, broadcast will use mnemonic from CLI args
+            vm.startBroadcast();
+        }
 
         ACTGovernor governor = new ACTGovernor(
             IVotes(actTokenAddress),
